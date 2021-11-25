05ccca638414f823b625474134b25314b03999b7
Jacques GOUDE <jacques.goude@icloud.com>
2021-11-25 14:39:25 +0000

    Adding image logo

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -12,6 +12,23 @@ class RegisterPageState extends State<RegisterPage> {
   final _formKey = GlobalKey<FormState>();
   bool _isSubmitting, _obscuredText = true;
   String _username, _email, _password;
+
+  Widget _showLogo() {
+    return Padding(
+        padding: EdgeInsets.only(top: 20.0),
+        child: Container(
+          height: 150.0,
+          width: 150.0,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/logorarecamion.png'),
+              fit: BoxFit.fill,
+            ),
+            shape: BoxShape.circle,
+          ),
+        ));
+  }
+
   Widget _showTitle() {
     return Text('Bienvenue', style: Theme.of(context).textTheme.headline1);
   }
@@ -101,11 +118,19 @@ class RegisterPageState extends State<RegisterPage> {
     http.Response response = await http.post(
         Uri.parse('http://localhost:1337/auth/local/register'),
         body: {"username": _username, "email": _email, "password": _password});
+
     final responseData = json.decode(response.body);
-    setState(() => _isSubmitting = false);
-    _showSuccessSnack();
-    _redirectUser();
-    print(responseData);
+    if (response.statusCode == 200) {
+      setState(() => _isSubmitting = false);
+      _showSuccessSnack();
+      _redirectUser();
+      print(responseData);
+    } else {
+      setState(() => _isSubmitting = false);
+      final String errorMsg = responseData['message'];
+
+      _showErrorSnack(errorMsg);
+    }
   }
 
   void _showSuccessSnack() {
@@ -115,6 +140,14 @@ class RegisterPageState extends State<RegisterPage> {
     _scaffoldKey.currentState.showSnackBar(snackbar);
 
     _formKey.currentState.reset();
+  }
+
+  void _showErrorSnack(String errorMsg) {
+    final snackbar =
+        SnackBar(content: Text(errorMsg, style: TextStyle(color: Colors.red)));
+    _scaffoldKey.currentState.showSnackBar(snackbar);
+
+    throw Exception('Erreur rencontrée: $errorMsg');
   }
 
   void _redirectUser() {
@@ -143,6 +176,7 @@ class RegisterPageState extends State<RegisterPage> {
               child: Form(
                   key: _formKey,
                   child: Column(children: [
+                    _showLogo(),
                     _showTitle(),
                     _showUsernameInput(),
                     _showEmailInput(),
