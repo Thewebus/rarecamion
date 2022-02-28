75caacc12fd057507594735b9b9d512e2e0f942c
Jacques GOUDE <jacques.goude@icloud.com>
2022-02-28 09:53:51 +0000

    MAJ 1 28/02/2022

--- lib/widgets/status_item.dart
+++ lib/widgets/status_item.dart
@@ -1,7 +1,6 @@
-import 'dart:convert';
+// ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:date_format/date_format.dart';
 import '../models/status_vehicule.dart';
@@ -22,13 +21,6 @@ class StatusItem extends StatelessWidget {
 
     final XFile photo = await _picker.pickImage(source: ImageSource.gallery);
 
-    /*
-    Map<String, String> headers = {
-      'Content-Type': 'application/json;charset=utf-8',
-      'Accept': 'multipart/mixed',
-    };
-    */
-
     final request = http.MultipartRequest(
         'POST', Uri.parse('http://rarecamion.com:1337/api/upload/'));
 
@@ -38,7 +30,7 @@ class StatusItem extends StatelessWidget {
 
     request.fields['ref'] = "api::status-vehicule.status-vehicule";
     request.fields['field'] = "image";
-    request.fields['refId'] = this.statusVehicule.id.toString();
+    request.fields['refId'] = statusVehicule.id.toString();
 
     final picture =
         http.MultipartFile.fromBytes('files', await photo.readAsBytes());
@@ -46,6 +38,18 @@ class StatusItem extends StatelessWidget {
     request.files.add(picture);
 
     final response = await request.send().then((response) => null);
+    final responseData = await response.stream.toBytes();
+
+    final result = String.fromCharCodes(responseData);
+
+    print(result);
+
+    /*
+    Map<String, String> headers = {
+      'Content-Type': 'application/json;charset=utf-8',
+      'Accept': 'multipart/mixed',
+    };
+    */
 
     /*
     http.Response response =
@@ -71,12 +75,6 @@ class StatusItem extends StatelessWidget {
     }
 
     */
-
-    final responseData = await response.stream.toBytes();
-
-    final result = String.fromCharCodes(responseData);
-
-    print(result);
   }
 
   @override
@@ -97,15 +95,12 @@ class StatusItem extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text('${this.statusVehicule.attributes.libelleStatus}',
-                        style: TextStyle(
-                            fontSize: 16,
-                            color: Color.fromARGB(255, 255, 255, 255))),
+                    Text('${statusVehicule.attributes.libelleStatus}',
+                        style: TextStyle(fontSize: 16, color: Colors.white)),
                     Text(
                         '${dtformat(this.statusVehicule.attributes.updatedAt)}',
                         style: TextStyle(
-                            fontSize: 9.0,
-                            color: Color.fromARGB(255, 194, 250, 89))),
+                            fontSize: 10.0, color: Colors.lightGreenAccent)),
                   ]),
               Row(
                 children: [
